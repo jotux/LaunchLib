ba78f79939feab27507705cdad735fc03b465631
Joe Brown <joepbrown@gmail.com>
2011-06-10 16:51:50 -0700

    Replace tabs with spaces.

--- timer.c
+++ timer.c
@@ -7,8 +7,8 @@ volatile uint32_t now = 0;
 void TimerAInit(void)
 {
     TACCTL0 &= ~CCIE;           // disable ints
-	TACCR0 = INT_CNT_DCO;       // set the overflow
-	TACTL = TASSEL_2 + MC_1;    // smclk, up to then overflow at ccr0
+    TACCR0 = INT_CNT_DCO;       // set the overflow
+    TACTL = TASSEL_2 + MC_1;    // smclk, up to then overflow at ccr0
     TACCTL0 = CCIE;             // enable ints
 }
 
