d655d71e6b85f7f07c738dbb1a22d6c29bb0bdbf
Jotux <joepbrown@gmail.com>
2012-03-01 20:22:50 -0800

    Update prototypes to make them more compliant.

--- src/pwm.h
+++ src/pwm.h
@@ -1,9 +1,9 @@
 #ifndef PWM_H
 #define PWM_H
 
-void PwmInit(uint8_t channel);
-void PwmSetPeriod(uint8_t channel, uint32_t frequency);
-void PwmSetDuty(uint8_t channel, uint8_t duty);
+extern void PwmInit(uint8_t channel);
+extern void PwmSetPeriod(uint8_t channel, uint32_t frequency);
+extern void PwmSetDuty(uint8_t channel, uint8_t duty);
 
 
 typedef struct
