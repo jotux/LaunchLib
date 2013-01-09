3d03514496dfb7a9d41f999d06eb6c72dba35c06
Joe Brown <joepbrown@gmail.com>
2013-01-08 22:13:43 -0800

    Update doc-automation batch file

--- doxygen_config/update_docs.bat
+++ doxygen_config/update_docs.bat
@@ -1,3 +1,11 @@
 doxygen ./doxy_launchlib
 cd ..
-git checkout /origin/gh_pages
+xcopy .\docs ..\docs /s /i
+git checkout gh-pages
+rmdir .\docs /s /q
+xcopy ..\docs .\docs /s /i
+rmdir ..\docs /s /q
+git add -u
+git add .
+git commit -m "Automatic doc update"
+git push
